cff4c3bbbda169da2f6c43066faf943c939ae7d5
Row0 <Row0@outlook.com>
2023-01-09 04:28:45 -0600

    Initial Commit

---
+++ Trimension/TrimensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrimensionApp.swift
+//  Trimension
+//
+//  Created by Shiyuan Liu on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct TrimensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
